1386f986e003dfb81a2f4c2e4121f96b2760726e
Eric Ace <24485843+aceeric@users.noreply.github.com>
2020-04-29 14:37:04 -0400

    Updated flycam  attribution

--- cmd/flycam/doc.go
+++ cmd/flycam/doc.go
@@ -1,5 +1,6 @@
 /*
 Provides a "Fly By" camera implementation on top of the G3N engine. Functionality:
+
  W       = Cam Forward (away from the viewer)
  A       = Strafe Left
  S       = Back (toward the viewer)
@@ -12,8 +13,10 @@ Provides a "Fly By" camera implementation on top of the G3N engine. Functionalit
  F12     = Unbind/bind keyboard/mouse from/to the sim window. Initially, controls are bound
  ESC     = Exit simulation
 
-Thanks to: https://github.com/JoeyDeVries/LearnOpenGL for the vector math supporting this functionality. According
-to the attribution page there, no license / copyright presentation is required if content from that source is
-used in the public domain.
+Thanks to: https://learnopengl.com/About for the vector math supporting mouse look functionality. Specifically:
+https://github.com/JoeyDeVries/LearnOpenGL/blob/master/includes/learnopengl/camera.h
+
+The referenced GitHub code is copyrighted by Joey de Vries (https://twitter.com/JoeyDeVriez) under
+license: https://creativecommons.org/licenses/by-nc/4.0/
 */
 package flycam
